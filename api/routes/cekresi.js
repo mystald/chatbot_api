f9aca86433611a74f075d0758ac630a056c5b322
Mystald <hilmanabdan28@gmail.com>
2020-06-09 16:30:13 +0700

    Changed JSON output format in get cekresi

--- api/routes/cekresi.js
+++ api/routes/cekresi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const request =  require('request');
+const request = require('request');
 
 router.get('/:noresi/:kurir', (req, res, next) => {
     let url = "http://api.shipping.esoftplay.com/waybill/"+req.params.noresi+"/"+req.params.kurir
@@ -14,18 +14,30 @@ router.get('/:noresi/:kurir', (req, res, next) => {
 
         if(!error && result.statusCode == 200){
             //console.log(result.body);
+            var chat = {}
             data = result.body;
-            res.status(200).json({
-                chats: [
-                    {
-                        text: data.result.summary.courier_name+"\n"+data.result.summary.waybill_number+
-                        "\nShipper : "+data.result.summary.shipper_name+"\nReceiver : "+data.result.summary.receiver_name+
-                        "\nOrigin : "+data.result.summary.origin+"\nDestination : "+data.result.summary.destination+
-                        "\nStatus : "+data.result.summary.status,
-                        type: "text"
-                    }
-                ]
+            chat["chats"] = [
+                {
+                    text: data.result.summary.courier_name+"\n"+data.result.summary.waybill_number+
+                    "\n\nShipper : "+data.result.summary.shipper_name+"\nReceiver : "+data.result.summary.receiver_name+
+                    "\nOrigin : "+data.result.summary.origin+"\nDestination : "+data.result.summary.destination+
+                    "\nStatus : "+data.result.summary.status,
+                    type: "text"
+                }
+            ];
+            chat["chats"].push({
+                text: "Manifests : ",
+                type: "text"
             });
+            for(var index in data.result.manifest){
+                val = data.result.manifest[index];
+                var content = {}
+                content["text"] = val.manifest_description+"\n"+val.manifest_date+"\n"+val.manifest_time;
+                content["type"] = "text";
+                console.log(content);
+                chat["chats"].push(content);
+            }
+            res.status(200).json(chat);
         };
     })
     console.log("Cek resi "+req.params.noresi+" "+req.params.kurir);
