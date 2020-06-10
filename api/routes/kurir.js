d6ee2c6e685c7091c0feb1ebf500a01b9e089f18
Mystald <hilmanabdan28@gmail.com>
2020-06-11 04:14:18 +0700

    Bugfix: synced kurir_slug and kurir_id

--- api/routes/kurir.js
+++ api/routes/kurir.js
@@ -2,6 +2,20 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
+let newurl = "http://api.shipping.esoftplay.com/courier_waybill";
+let newoptions = { json: "true", method: "GET" };
+request(newurl,newoptions,(error,result,body) => {
+    if(error){
+        console.log(error)
+    };
+
+    if(!error && result.statusCode == 200){
+        list_idKurir = result.body.result;
+    };
+});
+
+
+
 router.get('/cekresi', (req, res, next) => {
     let url = "http://api.shipping.esoftplay.com/couriers";
     let options = { json: "true", method: "GET" };
@@ -23,11 +37,22 @@ router.get('/cekresi', (req, res, next) => {
                 content["label"] = val.name;
                 content["type"] = "path";
                 content["path"] = "5edceaa520f8374bf65ac755";
+
+                idKurir = list_idKurir.filter(function(item) {
+                    return val.slug.includes(item);
+                });
+
+                if(idKurir[0] == null){
+                    idKurir[0] = "";
+                }
+
                 content["variable"] = {
-                    kurir: val.slug
+                    kurir: idKurir[0]
                 };
                 //console.log(content);
-                buttonArr.push(content);
+                if(idKurir != "" && !buttonArr.some(item => item.variable.kurir.includes(idKurir[0]))){
+                    buttonArr.push(content);
+                }
             };
             chat['chats'] = [{
                 text: "Silahkan pilih kurir yang tersedia dibawah",
@@ -66,12 +91,23 @@ router.get('/cekongkir', (req, res, next) => {
                 content["label"] = val.name;
                 content["type"] = "path";
                 content["path"] = "5ee13d6620f837185ebedc06";
+
+                idKurir = list_idKurir.filter(function(item) {
+                    return val.slug.includes(item);
+                });
+
+                if(idKurir[0] == null){
+                    idKurir[0] = "";
+                }
+
                 content["variable"] = {
-                    kurir: val.slug,
+                    kurir: idKurir[0],
                     kurir_det: val.name
                 };
                 //console.log(content);
-                buttonArr.push(content);
+                if(idKurir != "" && !buttonArr.some(item => item.variable.kurir.includes(idKurir[0]))){
+                    buttonArr.push(content);
+                }
             };
             chat['chats'] = [{
                 text: "Silahkan pilih kurir yang tersedia dibawah",
